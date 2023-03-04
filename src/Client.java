98dc2ddc2eb2c63e92976b98aff80d9d9b13027d
Shubham Raut <shubhamrau46@gmail.com>
2023-03-04 15:22:15 +0530

    Server code updated

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,77 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
+
+    Socket socket;
+    BufferedReader br;          // for reading data
+    PrintWriter out;            // for writing data
+    public Client(){
+    try {
+        System.out.println("Sending request to Server..");
+        Socket socket = new Socket("127.0.0.1", 7777); //[ ip and port ]
+        System.out.println("Connection done.");
+
+        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream());
+        startReading();
+        startWriting();
+    }catch(Exception e)
+    {
+        e.printStackTrace();
+        }
+    }
+
+    public void startReading()
+    {
+        // Thread will read and give the data
+        Runnable r1 = ()->{
+            System.out.println("Reader Started. ");
+            while(true)
+            {
+                try {
+                    String msg = br.readLine();     // it will come from client in case of server and viseversa
+                    if (msg.equals("exit")) {
+                        System.out.println("Server terminated the chat.");
+                        break;
+                    }
+                    System.out.println("Server: " + msg);
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // creating objects of thread
+        new Thread(r1).start();
+    }
+
+
+    public void startWriting()
+    {
+        // Thread will take data from user and will send it to the client
+        Runnable r2 = () -> {
+            System.out.println("Writer Started.");
+           while(true){ try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  // taking input from console
+                String content = br.readLine();
+                out.println(content);
+                out.flush();
+
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }}
+
+        };
+        new Thread(r2).start();
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("this is client.");
+        new Client();
+
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,87 @@
+import java.net.*;   // imported all networking classes to be used.
+import java.io.*;   // for using bufferreader
+
 public class Server {
+
+    // defining variables to send and receive data
+    ServerSocket server;        // for
+    Socket socket;              // received data will be stored here
+    BufferedReader br;          // for reading data
+    PrintWriter out;            // for writing data
+
+    // creating constructor
+    public Server()
+    {
+        // Almost all work will be here
+
+        try {
+            server = new ServerSocket(7777);  // if object is successfully created
+            System.out.println("Server is ready to accept connections..");
+            System.out.println("Waiting...");
+            socket = server.accept();  // accepting object of socket(from client) and storing in variable.
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void startReading()
+    {
+        // Thread will read and give the data
+        Runnable r1 = ()->{
+            System.out.println("Reader Started. ");
+            while(true)
+            {
+                try {
+                    String msg = br.readLine();     // it will come from client in case of server and viseversa
+                    if (msg.equals("exit")) {
+                        System.out.println("Client terminated the chat.");
+                        break;
+                    }
+                    System.out.println("Client: " + msg);
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // creating objects of thread
+        new Thread(r1).start();
+    }
+
+
+    public void startWriting()
+    {
+        // Thread will take data from user and will send it to the client
+        Runnable r2 = () -> {
+            System.out.println("Writer Started.");
+            while(true) {
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  // taking input from console
+                    String content = br.readLine();
+                    out.println(content);
+                    out.flush();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        new Thread(r2).start();
+
+    }
+    public static void main(String[] args)
+    {
+        // calling constructor
+        new Server();
+    }
 }
